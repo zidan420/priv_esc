416fb3ee2760f87d32c63dd57f43636a48e6af00
Md Zidan Khan <80000340+zidan420@users.noreply.github.com>
2022-01-22 12:05:24 +0600

    Add files via upload

---
+++ Unquoted_service_path.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // Get services that are NOT enclosed in Quotes
+    // Service Path has SPACEs or not
+    // User has WRITE ACCESS or not
+    // User has right to RESTART service or not OR auto-start on reboot?
+    return(0);
+}
---
+++ wipecpp.cpp
@@ -0,0 +1,9 @@
+#include "headers/quoted_service_path.h"
+#include "headers/auto_run.h"
+
+int main()
+{
+    qsp();
+    auto_run();
+    return 0;
+}
